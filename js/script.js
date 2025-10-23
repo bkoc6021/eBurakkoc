27f357db81bc1c99c701091c6052a2413c630326
Burak KOÇ <bkoc6021@gmail.com>
2025-10-24 01:10:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-let index = 0;
-const slides = document.querySelectorAll('.slider img');
-
-function showSlide(i) {
-  slides.forEach(s => s.style.display = 'none');
-  slides[i].style.display = 'block';
-}
-
-function nextSlide() {
-  index = (index + 1) % slides.length;
+$(document).ready(function(){
+  // Slider fonksiyonu
+  let index = 0;
+  const slides = $('.slider img');
+  function showSlide(i) {
+    slides.hide();
+    $(slides[i]).show();
+  }
+  function nextSlide() {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+  }
+  setInterval(nextSlide, 3000); // Her 3 saniyede bir slider geçişi
   showSlide(index);
-}
-
-setInterval(nextSlide, 3000);
-showSlide(index);
+});
